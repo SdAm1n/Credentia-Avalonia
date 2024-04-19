7919eaa1e72a664a5c600a821b47ffceeefd519a
Md. Sadikul Amin Sadman <anarchst49@gmail.com>
2024-04-19 23:15:03 +0600

    added fluent avalonia toolkit

--- Credentia/Views/MainWindow.axaml.cs
+++ Credentia/Views/MainWindow.axaml.cs
@@ -1,8 +1,9 @@
 using Avalonia.Controls;
+using FluentAvalonia.UI.Windowing;
 
 namespace Credentia.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : AppWindow
 {
     public MainWindow()
     {
